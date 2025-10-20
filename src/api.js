5ce94583fb5831eaf1ec176eebc7ba70a18dbfea
Akhil Pandey <akhilpandey494@gmail.com>
2025-10-20 22:54:00 +0530

    Update

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": 'API Key',
+    "X-RapidAPI-Key": 'd62c810199mshb8aba4ae0eb6639p15a729jsne658ec586cb4',
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "Api Key"; // enter your key from openweather API
+export const WEATHER_API_KEY = "59fc9340fdbbb5bf832e07c7e8a5b197"; // enter your key from openweather API
